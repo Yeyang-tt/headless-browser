6c563c8d67e7d62429b844822d069dd33209b3bd
Yeyang <15538844552@163.com>
2025-02-25 13:50:02 +0800

    添加依赖

---
+++ src/main/java/com/yeyang/browser/headless/util/CommandUtil.java
@@ -0,0 +1,44 @@
+package com.yeyang.browser.headless.util;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+
+@Slf4j
+@UtilityClass
+public class CommandUtil {
+
+    /**
+     * window
+     *
+     * @param command 命令
+     */
+    public void execWindow(String command) {
+        // Process可以控制该子进程的执行或获取该子进程的信息
+        Process process = null;
+        try {
+            // exec()方法指示Java虚拟机创建一个子进程执行指定的可执行程序，并返回与该子进程对应的Process对象实例。
+            process = Runtime.getRuntime().exec(command);
+            // 下面两个可以获取输入输出流
+            InputStream errorStream = process.getErrorStream();
+            InputStream inputStream = process.getInputStream();
+
+            // 等待子进程完成再往下执行，返回值是子线程执行完毕的返回值,返回0表示正常结束
+            int exitStatus = process.waitFor();
+            // 第二种接受返回值的方法
+            // 接收执行完毕的返回值
+            int i = process.exitValue();
+            log.debug("命令行工具-windows命令：{}：{}", command, i);
+            // if (i != 0) {
+            // 	windowExec(command);
+            // }
+        } catch (Exception e) {
+            log.error("命令行工具-windows命令异常：", e);
+        } finally {
+            if (process != null) {
+                process.destroy(); // 销毁子进程
+            }
+        }
+    }
+}
