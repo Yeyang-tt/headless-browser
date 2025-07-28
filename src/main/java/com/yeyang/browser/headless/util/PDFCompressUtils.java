57466218088388216bf0d44d61cb937998c12929
Yeyang <15538844552@163.com>
2025-07-28 14:52:22 +0800

    pdf compress

---
+++ src/main/java/com/yeyang/browser/headless/util/PDFCompressUtils.java
@@ -0,0 +1,18 @@
+package com.yeyang.browser.headless.util;
+
+import java.io.File;
+
+/**
+ * PDF压缩工具类
+ */
+public final class PDFCompressUtils {
+
+    // -- Fields --
+    private static float compQualDefault = 0f; //
+    private File input;
+    private File output;
+    private float compQual = -1;
+    private boolean tiff = false;
+
+
+}
